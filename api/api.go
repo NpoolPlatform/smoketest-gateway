a849749fc1aa3fa9a630ecb4c54d0afd84a95fbc
lidaqiang <dqli161@163.com>
2023-04-11 20:04:28 +0800

    delete

--- api/api.go
+++ api/api.go
@@ -1,8 +1,6 @@
 package api
 
 import (
-	"context"
-
 	testcasegw "github.com/NpoolPlatform/message/npool/smoketest/gw/v1/testcase"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 
@@ -17,8 +15,5 @@ func Register(server grpc.ServiceRegistrar) {
 	testcasegw.RegisterGatewayServer(server, &Server{})
 }
 func RegisterGateway(mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) error {
-	if err := testcasegw.RegisterGatewayHandlerFromEndpoint(context.Background(), mux, endpoint, opts); err != nil {
-		return err
-	}
 	return nil
 }
