6d9887301e393e87aec5895aedc72f8a1fd3bbf2
lidaqiang <dqli161@163.com>
2023-04-12 19:35:14 +0800

    reset GetTestCase

--- pkg/testcase/query.go
+++ pkg/testcase/query.go
@@ -2,6 +2,7 @@ package testcase
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
 	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
@@ -84,4 +85,51 @@ func GetTestCases(ctx context.Context, offset, limit int32) ([]*npool.TestCase, 
 	}
 
 	return _infos, total, nil
+}
+
+func (handler *Handler) GetTestCase(ctx context.Context) (*npool.TestCase, error) {
+	if handler.ID == nil {
+		return nil, fmt.Errorf("invalid testcase id")
+	}
+
+	info, err := testcasemwcli.GetTestCase(ctx, *handler.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	_api, err := apimwcli.GetAPIOnly(
+		ctx,
+		&apimgrpb.Conds{
+			ID: &commonpb.StringVal{
+				Op:    cruder.EQ,
+				Value: info.ApiID,
+			},
+		},
+	)
+	if err != nil {
+		logger.Sugar().Errorw("CreateTestCase", "err", err)
+		return nil, err
+	}
+
+	_info := &npool.TestCase{
+		ID:               info.ID,
+		Name:             info.Name,
+		ModuleID:         info.ModuleID,
+		ModuleName:       info.ModuleName,
+		ApiID:            info.ApiID,
+		ApiPath:          _api.Path,
+		ApiPathPrefix:    _api.PathPrefix,
+		ApiServiceName:   _api.ServiceName,
+		ApiProtocol:      _api.Protocol.String(),
+		ApiMethod:        _api.Method.String(),
+		ApiDeprecated:    _api.GetDepracated(),
+		Arguments:        info.Arguments,
+		Expectation:      info.ExpectationResult,
+		TestCaseType:     info.TestCaseType,
+		RelatedTestCases: []*relatedtestcase.RelatedTestCase{},
+		Deprecated:       info.Deprecated,
+		CreatedAt:        info.CreatedAt,
+		UpdatedAt:        info.UpdatedAt,
+	}
+	return _info, nil
 }
