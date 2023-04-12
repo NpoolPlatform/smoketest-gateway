72af6a50731bc5d1f44265bef1a47d6bd456bb0d
lidaqiang <dqli161@163.com>
2023-04-12 19:33:25 +0800

    update query

--- pkg/testcase/query.go
+++ pkg/testcase/query.go
@@ -2,17 +2,18 @@ package testcase
 
 import (
 	"context"
-	"fmt"
 
-	apimwcli "github.com/NpoolPlatform/basal-middleware/pkg/client/api"
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
 	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
-	commonpb "github.com/NpoolPlatform/message/npool"
-	apimgrpb "github.com/NpoolPlatform/message/npool/basal/mgr/v1/api"
 	"github.com/NpoolPlatform/message/npool/smoketest/gw/v1/relatedtestcase"
 	npool "github.com/NpoolPlatform/message/npool/smoketest/gw/v1/testcase"
 	testcasemgrpb "github.com/NpoolPlatform/message/npool/smoketest/mgr/v1/testcase"
 	testcasemwcli "github.com/NpoolPlatform/smoketest-middleware/pkg/client/testcase"
+
+	apimwcli "github.com/NpoolPlatform/basal-middleware/pkg/client/api"
+	apimgrpb "github.com/NpoolPlatform/message/npool/basal/mgr/v1/api"
+
+	commonpb "github.com/NpoolPlatform/message/npool"
 )
 
 func GetTestCases(ctx context.Context, offset, limit int32) ([]*npool.TestCase, uint32, error) {
@@ -38,35 +39,43 @@ func GetTestCases(ctx context.Context, offset, limit int32) ([]*npool.TestCase, 
 		testCaseIDs = append(testCaseIDs, info.ID)
 	}
 
-	fmt.Println("TestCaseIDs: ", testCaseIDs)
-	fmt.Println("ApiIDs: ", apiIDs)
+	apis, _, err := apimwcli.GetAPIs(ctx, &apimgrpb.Conds{
+		IDs: &commonpb.StringSliceVal{
+			Op:    cruder.IN,
+			Value: apiIDs,
+		},
+	}, int32(len(apiIDs)), 0)
+	if err != nil {
+		logger.Sugar().Errorw("GetTestCases", "err", err)
+		return nil, 0, err
+	}
+	apiMap := map[string]*apimgrpb.API{}
 
-	// 查询用例关联的前置条件|后置条件
-
-	// TODO:After
-
-	// 查询API信息
-
-	// TODO:After
+	for _, row := range apis {
+		apiMap[row.ID] = row
+	}
 
 	_infos := []*npool.TestCase{}
-	_relatedTestCases := []*relatedtestcase.RelatedTestCase{}
 	for _, info := range infos {
+		_api, ok := apiMap[info.ApiID]
+		if !ok {
+			continue
+		}
 		row := npool.TestCase{
 			ID:               info.ID,
 			Name:             info.Name,
 			ModuleID:         info.ModuleID,
 			ModuleName:       info.ModuleName,
 			ApiID:            info.ApiID,
-			ApiPath:          "",
-			ApiPathPrefix:    "",
-			ApiServiceName:   "",
-			ApiProtocol:      "",
-			ApiMethod:        "",
-			ApiDeprecated:    false,
+			ApiPath:          _api.Path,
+			ApiPathPrefix:    _api.PathPrefix,
+			ApiServiceName:   _api.ServiceName,
+			ApiProtocol:      _api.Protocol.String(),
+			ApiMethod:        _api.Method.String(),
+			ApiDeprecated:    _api.Depracated,
 			Arguments:        info.Arguments,
 			TestCaseType:     info.TestCaseType,
-			RelatedTestCases: _relatedTestCases,
+			RelatedTestCases: []*relatedtestcase.RelatedTestCase{},
 			Deprecated:       info.Deprecated,
 			CreatedAt:        info.CreatedAt,
 			UpdatedAt:        info.UpdatedAt,
@@ -75,50 +84,4 @@ func GetTestCases(ctx context.Context, offset, limit int32) ([]*npool.TestCase, 
 	}
 
 	return _infos, total, nil
-}
-
-func (handler *Handler) GetTestCase(ctx context.Context) (*npool.TestCase, error) {
-	if handler.ID == nil {
-		return nil, fmt.Errorf("invalid testcase id")
-	}
-
-	info, err := testcasemwcli.GetTestCase(ctx, *handler.ID)
-	if err != nil {
-		return nil, err
-	}
-
-	_api, err := apimwcli.GetAPIOnly(
-		ctx,
-		&apimgrpb.Conds{
-			ID: &commonpb.StringVal{
-				Op:    cruder.EQ,
-				Value: info.ApiID,
-			},
-		},
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	_info := &npool.TestCase{
-		ID:               info.ID,
-		Name:             info.Name,
-		ModuleID:         info.ModuleID,
-		ModuleName:       info.ModuleName,
-		ApiID:            info.ApiID,
-		ApiPath:          _api.Path,
-		ApiPathPrefix:    _api.PathPrefix,
-		ApiServiceName:   _api.ServiceName,
-		ApiProtocol:      _api.Protocol.String(),
-		ApiMethod:        _api.Method.String(),
-		ApiDeprecated:    _api.GetDepracated(),
-		Arguments:        info.Arguments,
-		Expectation:      info.ExpectationResult,
-		TestCaseType:     info.TestCaseType,
-		RelatedTestCases: []*relatedtestcase.RelatedTestCase{},
-		Deprecated:       info.Deprecated,
-		CreatedAt:        info.CreatedAt,
-		UpdatedAt:        info.UpdatedAt,
-	}
-	return _info, nil
 }
