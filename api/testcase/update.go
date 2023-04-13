052f3b9c1b1bd54bdbbcec2839d91d166bd2be82
lidaqiang <dqli161@163.com>
2023-04-13 11:54:46 +0800

    add udpate in gw

---
+++ api/testcase/update.go
@@ -0,0 +1,47 @@
+package testcase
+
+import (
+	"context"
+
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	npool "github.com/NpoolPlatform/message/npool/smoketest/gw/v1/testcase"
+	testcase1 "github.com/NpoolPlatform/smoketest-gateway/pkg/testcase"
+
+	"github.com/gogo/status"
+	"google.golang.org/grpc/codes"
+)
+
+func (s *Server) UpdateTestCase(ctx context.Context, in *npool.UpdateTestCaseRequest) (*npool.UpdateTestCaseResponse, error) {
+	handler, err := testcase1.NewHandler(
+		ctx,
+		testcase1.WithID(&in.ID),
+		testcase1.WithName(in.Name),
+		testcase1.WithDescription(in.Description),
+		testcase1.WithArguments(in.Arguments),
+		testcase1.WithExpectationResult(in.Expectation),
+		testcase1.WithDeprecated(in.Deprecated),
+		testcase1.WithTestCaseType(in.TestCaseType),
+	)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"UpdateTestCase",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.UpdateTestCaseResponse{}, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	info, err := handler.UpdateTestCase(ctx)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"UpdateTestCase",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.UpdateTestCaseResponse{}, status.Error(codes.Internal, err.Error())
+	}
+
+	return &npool.UpdateTestCaseResponse{
+		Info: info,
+	}, nil
+}
---
+++ pkg/testcase/update.go
@@ -0,0 +1,41 @@
+package testcase
+
+import (
+	"context"
+
+	npool "github.com/NpoolPlatform/message/npool/smoketest/gw/v1/testcase"
+	testcasemwpb "github.com/NpoolPlatform/message/npool/smoketest/mw/v1/testcase"
+	testcasemwcli "github.com/NpoolPlatform/smoketest-middleware/pkg/client/testcase"
+)
+
+type updateHandler struct {
+	*Handler
+}
+
+func (h *Handler) UpdateTestCase(ctx context.Context) (*npool.TestCase, error) {
+	_, err := testcasemwcli.UpdateTestCase(ctx, &testcasemwpb.UpdateTestCaseRequest{
+		Info: &testcasemwpb.CreateTestCaseReq{
+			ID:                *h.ID,
+			Name:              *h.Name,
+			Description:       h.Description,
+			Arguments:         *h.Arguments,
+			ExpectationResult: *h.ExpectationResult,
+			TestCaseType:      h.TestCaseType,
+			Deprecated:        *h.Deprecated,
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	handler := &updateHandler{
+		Handler: h,
+	}
+
+	info, err := handler.GetTestCase(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return info, nil
+}
