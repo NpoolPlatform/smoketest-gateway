56f3ab0020ef736a31305fe05150d282f1fae3ef
daqiangli <dqli161@163.com>
2023-04-21 11:12:49 +0800

    transfer ID

--- pkg/module/create.go
+++ pkg/module/create.go
@@ -6,6 +6,7 @@ import (
 
 	npool "github.com/NpoolPlatform/message/npool/smoketest/mgr/v1/module"
 	modulecli "github.com/NpoolPlatform/smoketest-middleware/pkg/client/module"
+	"github.com/google/uuid"
 )
 
 func (h *Handler) CreateModule(ctx context.Context) (*npool.Module, error) {
@@ -13,9 +14,13 @@ func (h *Handler) CreateModule(ctx context.Context) (*npool.Module, error) {
 		return nil, fmt.Errorf("invalid name")
 	}
 
+	id := uuid.NewString()
+	h.ID = &id
+
 	if _, err := modulecli.CreateModule(
 		ctx,
 		&npool.ModuleReq{
+			ID:          h.ID,
 			Name:        h.Name,
 			Description: h.Description,
 		},
