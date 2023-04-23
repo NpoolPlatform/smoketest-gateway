8b81869da94fa0202f4b89a56fa61df7d13c788a
daqiangli <dqli161@163.com>
2023-04-23 15:13:16 +0800

    add update module

---
+++ api/module/update.go
@@ -0,0 +1,43 @@
+package module
+
+import (
+	"context"
+
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	npool "github.com/NpoolPlatform/message/npool/smoketest/gw/v1/module"
+	module1 "github.com/NpoolPlatform/smoketest-gateway/pkg/module"
+
+	"github.com/gogo/status"
+	"google.golang.org/grpc/codes"
+)
+
+func (s *Server) UpdateModule(ctx context.Context, in *npool.UpdateModuleRequest) (*npool.UpdateModuleResponse, error) {
+	handler, err := module1.NewHandler(
+		ctx,
+		module1.WithID(&in.ID),
+		module1.WithName(in.Name),
+		module1.WithDescription(in.Description),
+	)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"UpdateModule",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.UpdateModuleResponse{}, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	info, err := handler.UpdateModule(ctx)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"UpdateModule",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.UpdateModuleResponse{}, status.Error(codes.Internal, err.Error())
+	}
+
+	return &npool.UpdateModuleResponse{
+		Info: info,
+	}, nil
+}
---
+++ pkg/module/update.go
@@ -0,0 +1,23 @@
+package module
+
+import (
+	"context"
+
+	npool "github.com/NpoolPlatform/message/npool/smoketest/mgr/v1/module"
+	cli "github.com/NpoolPlatform/smoketest-middleware/pkg/client/module"
+)
+
+func (h *Handler) UpdateModule(ctx context.Context) (*npool.Module, error) {
+	if _, err := cli.UpdateModule(
+		ctx,
+		&npool.ModuleReq{
+			ID:          h.ID,
+			Name:        h.Name,
+			Description: h.Description,
+		},
+	); err != nil {
+		return nil, err
+	}
+
+	return h.GetModule(ctx)
+}
