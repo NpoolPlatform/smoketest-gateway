f10176aa684b9d4b2b22793f4d9ccef81396ed84
Zhao KK <kikakkz@hotmail.com>
2023-10-17 06:42:27 +0000

    Add testcase class

--- pkg/testcase/query.go
+++ pkg/testcase/query.go
@@ -2,48 +2,47 @@ package testcase
 
 import (
 	"context"
-	"fmt"
 
+	apimwcli "github.com/NpoolPlatform/basal-middleware/pkg/client/api"
 	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
-	npool "github.com/NpoolPlatform/message/npool/smoketest/gw/v1/testcase"
-	pb "github.com/NpoolPlatform/message/npool/smoketest/mw/v1/testcase"
-	cli "github.com/NpoolPlatform/smoketest-middleware/pkg/client/testcase"
-
-	apicli "github.com/NpoolPlatform/basal-middleware/pkg/client/api"
-	apipb "github.com/NpoolPlatform/message/npool/basal/mw/v1/api"
+	apimwpb "github.com/NpoolPlatform/message/npool/basal/mw/v1/api"
 	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
+	npool "github.com/NpoolPlatform/message/npool/smoketest/gw/v1/testcase"
+	testcasemwpb "github.com/NpoolPlatform/message/npool/smoketest/mw/v1/testcase"
+	testcasemwcli "github.com/NpoolPlatform/smoketest-middleware/pkg/client/testcase"
 )
 
 type queryHandler struct {
 	*Handler
-	Infos []*pb.TestCase
+	testCases []*testcasemwpb.TestCase
+	infos     []*npool.TestCase
+	apis      map[string]*apimwpb.API
 }
 
-func (h *queryHandler) formalize(ctx context.Context) ([]*npool.TestCase, error) {
+func (h *queryHandler) getAPIs(ctx context.Context) error {
 	apiIDs := []string{}
-	for _, info := range h.Infos {
+	for _, info := range h.testCases {
 		apiIDs = append(apiIDs, info.ApiID)
 	}
-
-	apis, _, err := apicli.GetAPIs(ctx, &apipb.Conds{
+	apis, _, err := apimwcli.GetAPIs(ctx, &apimwpb.Conds{
 		EntIDs: &basetypes.StringSliceVal{Op: cruder.IN, Value: apiIDs},
 	}, 0, int32(len(apiIDs)))
 	if err != nil {
-		return nil, err
+		return err
 	}
-
-	apiMap := map[string]*apipb.API{}
-	for _, row := range apis {
-		apiMap[row.EntID] = row
+	for _, api := range apis {
+		h.apis[api.EntID] = api
 	}
+	return nil
+}
 
-	infos := []*npool.TestCase{}
-	for _, info := range h.Infos {
-		api, ok := apiMap[info.ApiID]
+func (h *queryHandler) formalize(ctx context.Context) {
+	for _, info := range h.testCases {
+		api, ok := h.apis[info.ApiID]
 		if !ok {
 			continue
 		}
-		row := npool.TestCase{
+		_info := npool.TestCase{
 			ID:             info.ID,
 			Name:           info.Name,
 			Description:    info.Description,
@@ -61,68 +60,57 @@ func (h *queryHandler) formalize(ctx context.Context) ([]*npool.TestCase, error)
 			Expectation:    info.Expectation,
 			OutputDesc:     info.OutputDesc,
 			TestCaseType:   info.TestCaseType,
+			TestCaseClass:  info.TestCaseClass,
 			Deprecated:     info.Deprecated,
 			CreatedAt:      info.CreatedAt,
 			UpdatedAt:      info.UpdatedAt,
 		}
-		infos = append(infos, &row)
+		h.infos = append(h.infos, &_info)
 	}
-
-	return infos, nil
 }
 
 func (h *Handler) GetTestCases(ctx context.Context) ([]*npool.TestCase, uint32, error) {
-	handler := &queryHandler{
-		Handler: h,
+	conds := &testcasemwpb.Conds{}
+	if h.ModuleID != nil {
+		conds.ModuleID = &basetypes.StringVal{Op: cruder.EQ, Value: *h.ModuleID}
 	}
-
-	conds := &pb.Conds{}
-	if handler.ModuleID != nil {
-		conds.ModuleID = &basetypes.StringVal{Op: cruder.EQ, Value: *handler.ModuleID}
-	}
-
-	infos, total, err := cli.GetTestCases(ctx, conds, handler.Offset, handler.Limit)
+	infos, total, err := testcasemwcli.GetTestCases(ctx, conds, h.Offset, h.Limit)
 	if err != nil {
 		return nil, 0, err
 	}
-
 	if len(infos) == 0 {
 		return nil, 0, nil
 	}
 
-	handler.Infos = infos
-
-	_infos, err := handler.formalize(ctx)
-	if err != nil {
+	handler := &queryHandler{
+		Handler:   h,
+		testCases: infos,
+	}
+	if err := handler.getAPIs(ctx); err != nil {
 		return nil, 0, err
 	}
-
-	return _infos, total, nil
+	handler.formalize(ctx)
+	return handler.infos, total, nil
 }
 
 func (h *Handler) GetTestCase(ctx context.Context) (*npool.TestCase, error) {
-	if h.ID == nil {
-		return nil, fmt.Errorf("invalid id")
-	}
-
-	info, err := cli.GetTestCase(ctx, *h.ID)
+	info, err := testcasemwcli.GetTestCase(ctx, *h.ID)
 	if err != nil {
 		return nil, err
 	}
 
 	handler := &queryHandler{
-		Handler: h,
+		Handler:   h,
+		testCases: []*testcasemwpb.TestCase{info},
 	}
-
-	handler.Infos = []*pb.TestCase{info}
-
-	_infos, err := handler.formalize(ctx)
-	if err != nil {
+	if err := handler.getAPIs(ctx); err != nil {
 		return nil, err
 	}
-	if len(_infos) == 0 {
+
+	handler.formalize(ctx)
+	if len(handler.infos) == 0 {
 		return nil, nil
 	}
 
-	return _infos[0], nil
+	return handler.infos[0], nil
 }
