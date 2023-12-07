6f1916cfeb885b004f854d362ecbd2b4ef79ee59
jakys <jakysing@163.com>
2023-12-07 09:32:41 +0000

    Fix http://47.116.78.131:10083/issues/2446

--- pkg/testplan/update.go
+++ pkg/testplan/update.go
@@ -3,12 +3,25 @@ package testplan
 import (
 	"context"
 
+	cruder "github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
 	npool "github.com/NpoolPlatform/message/npool/smoketest/gw/v1/testplan"
 	pb "github.com/NpoolPlatform/message/npool/smoketest/mw/v1/testplan"
 	cli "github.com/NpoolPlatform/smoketest-middleware/pkg/client/testplan"
 )
 
 func (h *Handler) UpdateTestPlan(ctx context.Context) (*npool.TestPlan, error) {
+	info, err := cli.GetTestPlanOnly(ctx, &pb.Conds{
+		ID:    &basetypes.Uint32Val{Op: cruder.EQ, Value: *h.ID},
+		EntID: &basetypes.StringVal{Op: cruder.EQ, Value: *h.EntID},
+	})
+	if err != nil {
+		return nil, err
+	}
+	if info == nil {
+		return nil, nil
+	}
+
 	if _, err := cli.UpdateTestPlan(ctx, &pb.TestPlanReq{
 		ID:          h.ID,
 		Name:        h.Name,
