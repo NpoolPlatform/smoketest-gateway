50ffa3f7f2f3114f0d43c0a78a45e22bf41f07a4
jakys <jakysing@163.com>
2023-12-07 09:36:34 +0000

    Fix http://47.116.78.131:10083/issues/2447

--- pkg/testplan/delete.go
+++ pkg/testplan/delete.go
@@ -3,19 +3,28 @@ package testplan
 import (
 	"context"
 
+	cruder "github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
 	npool "github.com/NpoolPlatform/message/npool/smoketest/gw/v1/testplan"
+	pb "github.com/NpoolPlatform/message/npool/smoketest/mw/v1/testplan"
 	testplanmwcli "github.com/NpoolPlatform/smoketest-middleware/pkg/client/testplan"
 )
 
 func (h *Handler) DeleteTestPlan(ctx context.Context) (*npool.TestPlan, error) {
-	info, err := h.GetTestPlan(ctx)
+	info, err := testplanmwcli.GetTestPlanOnly(ctx, &pb.Conds{
+		ID:    &basetypes.Uint32Val{Op: cruder.EQ, Value: *h.ID},
+		EntID: &basetypes.StringVal{Op: cruder.EQ, Value: *h.EntID},
+	})
 	if err != nil {
 		return nil, err
+	}
+	if info == nil {
+		return nil, nil
 	}
 
 	if _, err := testplanmwcli.DeleteTestPlan(ctx, *h.ID); err != nil {
 		return nil, err
 	}
 
-	return info, nil
+	return h.GetTestPlanExt(ctx, info)
 }
