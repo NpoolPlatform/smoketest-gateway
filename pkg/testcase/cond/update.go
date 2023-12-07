04b5f2229301123d81c31897f429ae726804d799
jakys <jakysing@163.com>
2023-12-07 09:29:54 +0000

    Fix http://47.116.78.131:10083/issues/2443

--- pkg/testcase/cond/update.go
+++ pkg/testcase/cond/update.go
@@ -3,11 +3,25 @@ package cond
 import (
 	"context"
 
+	cruder "github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
 	npool "github.com/NpoolPlatform/message/npool/smoketest/mw/v1/testcase/cond"
+	pb "github.com/NpoolPlatform/message/npool/smoketest/mw/v1/testcase/cond"
 	cli "github.com/NpoolPlatform/smoketest-middleware/pkg/client/testcase/cond"
 )
 
 func (h *Handler) UpdateCond(ctx context.Context) (*npool.Cond, error) {
+	info, err := cli.GetCondOnly(ctx, &pb.Conds{
+		ID:    &basetypes.Uint32Val{Op: cruder.EQ, Value: *h.ID},
+		EntID: &basetypes.StringVal{Op: cruder.EQ, Value: *h.EntID},
+	})
+	if err != nil {
+		return nil, err
+	}
+	if info == nil {
+		return nil, nil
+	}
+
 	if _, err := cli.UpdateCond(ctx, &npool.CondReq{
 		ID:          h.ID,
 		CondType:    h.CondType,
